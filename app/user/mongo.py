a4cb45100f90ec169e8de3f20b4385ecf8b3e1a1
aramis2008Home <seca@yy.er>
2025-02-09 13:45:58 +0300

    start mongoengine

---
+++ app/user/mongo.py
@@ -0,0 +1,13 @@
+from mongoengine.fields import StringField, DateTimeField, EmailField
+from mongoengine import connect, Document
+import datetime
+
+connect('apidb', host='localhost', port=27017)
+
+class User(Document):
+    email = EmailField(required=True, max_length=200)
+    password = StringField(required=True, max_length=200)
+    sign_up_datetime = DateTimeField(default=datetime.datetime.now)
+
+ross = User(email='ross@example.com', password='Ross').save()
+print(ross)
